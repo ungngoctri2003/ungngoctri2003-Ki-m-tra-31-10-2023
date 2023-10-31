949374e9826228198b8c3b294147bff01ff0a3cd
Ứng Trí <lamcongtri200@gmail.com>
2023-10-31 12:33:19 +0700

    update

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, useColorScheme } from 'react-native';
-import Index from './src/index'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Detail from './src/screens/Detail';
+import Navigator from './src/screens/Navigator';
+
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-      <Index/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Navigator" component={Navigator}  options={{ headerShown: false }}/>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="Detail" component={Detail}  options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
